aeb5c07cb2c82fdd46cedb52c8db8478dacb8834
Yegor <ukstudio.yegor@mail.ru>
2023-07-03 00:48:36 +0600

    Сделали задачу про Машу

---
+++ Example005_Hello_Masha/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя");
+string userName = Console.ReadLine();
+
+if (userName.ToLower() == "маша") // ToLower - переводит все буквы в нижний регистр
+{
+    Console.WriteLine("Ура, это же МАША!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(userName);
+}
