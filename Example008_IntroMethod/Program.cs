37380e475f35908548922fd77b5e407c0e9fb009
Yegor <ukstudio.yegor@mail.ru>
2023-07-06 17:18:39 +0600

    After Lection 2

---
+++ Example008_IntroMethod/Program.cs
@@ -0,0 +1,28 @@
+﻿int Max (int arg1, int arg2, int arg3){
+
+    int result = arg1;
+
+    if (arg2>result) result = arg2;
+    if (arg3>result) result = arg3;
+    return result;
+
+}
+
+int a1 = 21;
+int b1 = 34;
+int c1 = 41;
+int a2 = 2;
+int b2 = 4;
+int c2 = 122;
+int a3 = 22;
+int b3 = 31;
+int c3 = 44;
+
+// int max1 = Max(a1, b1, c1);
+// int max2 = Max(a2, b2, c2);
+// int max3 = Max(a3, b3, c3);
+
+int max = Max(Max(a1, b1, c1), Max(a2, b2, c2), Max(a3, b3, c3));
+
+Console.WriteLine(max);
+
---
+++ Example010_MethodArray/Program.cs
@@ -0,0 +1,15 @@
+﻿int[] array = {12, 24, 32, 43, 54, 26, 74, 82};
+
+int n = array.Length;
+int find = 43;
+int index = 0;
+
+while (index<n){
+
+    if (array[index] == find){
+        Console.WriteLine(index);
+        break; //оператор "Прерви". Закончить выполнение цикла при достижении этой строчки
+    }
+    
+    index = index+1;
+}
